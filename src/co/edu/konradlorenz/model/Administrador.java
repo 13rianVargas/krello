6d16812f1d340983cc125f3c7267509a2af3c8ec
Alexander Chacon <fresnedaalexander@gmail.com>
2024-09-14 23:31:15 -0500

    Desarrollo model
    Clase Persona, Administrador y Colaborador

---
+++ src/co/edu/konradlorenz/model/Administrador.java
@@ -0,0 +1,14 @@
+package co.edu.konradlorenz.model;
+
+public class Administrador extends Persona {
+
+	public Administrador() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Administrador(String nombre, String correo, String rol) {
+		super(nombre, correo, rol);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/co/edu/konradlorenz/model/Colaborador.java
@@ -0,0 +1,14 @@
+package co.edu.konradlorenz.model;
+
+public class Colaborador extends Persona {
+
+	public Colaborador() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Colaborador(String nombre, String correo, String rol) {
+		super(nombre, correo, rol);
+		// TODO Auto-generated constructor stub
+	}
+
+}
