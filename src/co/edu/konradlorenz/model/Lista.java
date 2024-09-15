db637599a37077821bdadab7b30023e5560ab9cd
13rian Vargas <bsvargasc13@gmail.com>
2024-09-15 00:24:40 -0500

    Model Update
    Añado clase Tablero, Lista y Tarea según el UML de Krello.
    Nota: Tarea muestra errores por falta de la clase "Colaborador".

--- src/co/edu/konradlorenz/model/Lista.java
+++ src/co/edu/konradlorenz/model/Lista.java
@@ -1,5 +1,38 @@
 package co.edu.konradlorenz.model;
 
-public class Lista {
+import java.util.ArrayList;
 
+public class Lista {
+    private String nombreLista;
+    private ArrayList<Lista> listaDeTareas = new ArrayList<>();
+    
+    public Lista() {
+    }
+    
+    public Lista(String nombreLista, ArrayList<Lista> listaDeTareas) {
+        this.nombreLista = nombreLista;
+        this.listaDeTareas = listaDeTareas;
+    }
+
+    public String getNombreLista() {
+        return nombreLista;
+    }
+
+    public void setNombreLista(String nombreLista) {
+        this.nombreLista = nombreLista;
+    }
+
+    public ArrayList<Lista> getListaDeTareas() {
+        return listaDeTareas;
+    }
+
+    public void setListaDeTareas(ArrayList<Lista> listaDeTareas) {
+        this.listaDeTareas = listaDeTareas;
+    }
+
+    @Override
+    public String toString() {
+        return "Lista [nombreLista=" + nombreLista + ", listaDeTareas=" + listaDeTareas + "]";
+    }
+    
 }
