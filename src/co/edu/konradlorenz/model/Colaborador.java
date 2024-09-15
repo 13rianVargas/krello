e2698bcd58ec22d712221fe4058cb2f04577eb22
13rian Vargas <bsvargasc13@gmail.com>
2024-09-15 09:48:24 -0500

    Elimino TODOs autogenerados

--- src/co/edu/konradlorenz/model/Colaborador.java
+++ src/co/edu/konradlorenz/model/Colaborador.java
@@ -3,12 +3,10 @@ package co.edu.konradlorenz.model;
 public class Colaborador extends Persona {
 
 	public Colaborador() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public Colaborador(String nombre, String correo, String rol) {
 		super(nombre, correo, rol);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
