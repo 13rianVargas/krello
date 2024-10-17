5fb325223e1cdf8f4bcd712f57d28a3d424fd8bb
13rian Vargas <bsvargasc13@gmail.com>
2024-10-17 15:13:58 -0500

    IN DEV - GUI
    >>AplMain:
    - Agrego método para ejecutar la GUI.
    - Comento el objControlador para no ejecutar la consola.
    
    >>Ventana:
    - Agrego el primer JFrame del menu principal en blanco.

--- src/co/edu/konradlorenz/controller/AplMain.java
+++ src/co/edu/konradlorenz/controller/AplMain.java
@@ -1,10 +1,22 @@
 package co.edu.konradlorenz.controller;
 
+import co.edu.konradlorenz.view.gui.Ventana;
+
 public class AplMain {
 
 	public static void main(String[] args) {
+		mostrarGUI();
+		
+		/*/ <- Quita el * entre las barras para descomentar la sección.
 		Controlador objControlador = new Controlador();
 		objControlador.run();
+		//*/
 	}
-
+	
+	// <- Agrega un * entre las barras para comentar el método.
+	// Solo creo este método para hacer visible la GUI y no interferir en el controller aún.
+	public static void mostrarGUI() {
+		Ventana.guiMenuPrincipal();
+	}
+	//*/
 }
