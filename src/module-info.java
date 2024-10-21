502b472679984eee713a57b12ebc098e9f45d356
13rian Vargas <bsvargasc13@gmail.com>
2024-10-21 00:48:26 -0500

    FIX - module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,8 @@
-/**
- * 
- */
-/**
- * 
- */
 module krello {
+	exports co.edu.konradlorenz.model;
+	exports co.edu.konradlorenz.view;
+	exports co.edu.konradlorenz.view.gui;
+	exports co.edu.konradlorenz.controller;
+
 	requires java.desktop;
 }
