a5107e80d11364a224ad7c51f9e5946d69e84129
13rian Vargas <bsvargasc13@gmail.com>
2024-11-05 22:04:46 -0500

    IN DEV - Base de Login

--- src/co/edu/konradlorenz/controller/AplMain.java
+++ src/co/edu/konradlorenz/controller/AplMain.java
@@ -1,12 +1,13 @@
 package co.edu.konradlorenz.controller;
 
-import co.edu.konradlorenz.view.gui.Principal;
+import co.edu.konradlorenz.view.gui.*;
 
 public class AplMain {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("unused")
-		Principal menuPrincipal = new Principal();
+		
+		Login login = new Login();
+		//Principal menuPrincipal = new Principal();
 		
 		/*/ <- Quita el * entre las barras para descomentar la sección.
 		Controlador objControlador = new Controlador();
--- src/co/edu/konradlorenz/view/gui/Login.java
+++ src/co/edu/konradlorenz/view/gui/Login.java
@@ -1,5 +1,70 @@
 package co.edu.konradlorenz.view.gui;
 
-public class Login {
+import java.awt.Color;
 
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class Login extends JFrame {
+	
+	JFrame login = new JFrame();
+	
+	//JPanel invisiblePanel;
+	JPanel pnlLoginHead;
+	JPanel pnlLoginbody;
+	ImageIcon imgKrelloLogo;
+	JLabel lblLogin;
+	JTextField txtLoginCorreo;
+	JTextField txtLoginNombre;
+	JPasswordField pwdContraseña;
+	JPasswordField pwdReContraseña;
+	JButton btnLoginContinuar;
+	JButton btnLoginPanelCrearCuenta;
+	JButton btnLoginCrearCuenta;
+	JButton btnLoginRegresar;
+	
+	public Login () {
+		
+		login.setDefaultCloseOperation(EXIT_ON_CLOSE);//Terminar la ejecución si se cierra la ventana
+		login.setTitle("KRELLO");//Título de la ventana
+		login.setSize(405,720);
+		login.setLocationRelativeTo(null);//Centra la ventana
+		login.setLayout(null);//Desactiva el control automático de paneles
+		login.setResizable(false);//No permite modificar el tamaño de la ventana
+		login.setBackground(Color.MAGENTA);//Color de la barra de la ventana
+		
+		pnlLoginHead = pnlLoginHead();
+		//pnlLoginbody = pnlLoginbody();
+		
+		login.add(pnlLoginHead);
+		//login.add(pnlLoginbody);
+		
+		login.setVisible(true);
+	}
+	
+	private JPanel pnlLoginHead() {
+		JPanel pnlLoginHead = new JPanel();
+		pnlLoginHead.setLayout(null);//Desactiva el control automático de paneles	   
+		pnlLoginHead.setBounds(0, 0, 405, 100);//x,y,ancho,altura
+		pnlLoginHead.setBackground(Color.CYAN);
+		pnlLoginHead.setOpaque(true);//false transparente
+		
+			JLabel lblKrelloLogo = new JLabel();
+			lblKrelloLogo.setIcon(new ImageIcon(getClass().getResource("/co/edu/konradlorenz/view/img/KrelloLogo187x60.png")));
+			lblKrelloLogo.setBounds(0, 20, 405, 60);
+			lblKrelloLogo.setHorizontalAlignment(SwingConstants.CENTER);//Centrar Horizontalmente
+	        
+			pnlLoginHead.add(lblKrelloLogo);
+	        
+		return pnlLoginHead;
+	}
+
+	private JPanel pnlLoginbody() {
+		
+		
+		
+		return null;
+	}
+
+	
 }
