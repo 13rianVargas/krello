1794751bbb19ec3b231c07c1f8b5e9d3efd789c9
13rian Vargas <bsvargasc13@gmail.com>
2024-11-05 22:20:33 -0500

    IN DEV - Base del AplMain

--- src/co/edu/konradlorenz/controller/AplMain.java
+++ src/co/edu/konradlorenz/controller/AplMain.java
@@ -1,12 +1,14 @@
 package co.edu.konradlorenz.controller;
 
-import co.edu.konradlorenz.view.gui.Principal;
+import co.edu.konradlorenz.view.gui.*;
 
 public class AplMain {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("unused")
-		Principal menuPrincipal = new Principal();
+		
+		//Principal framePrincipal = new Principal();
+		//Login frameLogin = new Login();
+		Tablero frameTablero = new Tablero();
 		
 		/*/ <- Quita el * entre las barras para descomentar la sección.
 		Controlador objControlador = new Controlador();
